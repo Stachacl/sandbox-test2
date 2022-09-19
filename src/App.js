94d27ce9cf8e1a9f5b1751c41984e096a4a3eafe
Stacha <93822077+Stachacl@users.noreply.github.com>
2022-09-19 23:14:26 +1000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <h2>I'm testing CodeSandbox today!</h2>
+    </div>
+  );
+}
